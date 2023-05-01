ff6d086440620c7b63fa05e02864863a7d23f0bf
harshitsingh001 <harshitsingh339658@gmail.com>
2023-05-01 17:05:54 +0530

    commited

--- Comparison.java
+++ Comparison.java
@@ -5,7 +5,7 @@ public class Comparison {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s1="java";
-		String s2="jav";
+		String s2="java";
 		
 		System.out.println(s1==s2);
 
--- Constructer.java
+++ Constructer.java
@@ -1,9 +1,35 @@
 package firstpackage;
 
+
+
+class First {
+	int a;
+	int b;
+	int d;
+	int c;
+	First(int a,int b){
+       this.a=a;
+       this.b=b;
+	}
+	
+	First(First s){
+		c=s.a;
+		d=s.b;
+	}
+	
+	
+	void display() {
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
 public class Constructer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		First first=new First(10,20);
+		First first2=new First(first); 
+		first2.display();
 
 	}
 
