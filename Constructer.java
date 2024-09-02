9cebd0a7097dd4d077381a13b35ca54b38a91c44
harshitsingh0001 <658harshitsingh@gmail.com>
2024-09-02 23:11:56 +0530

    chnages squashed

--- Constructer.java
+++ Constructer.java
@@ -4,7 +4,12 @@ public class Constructer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Shivani");
+		System.out.println("Shivani met harshit on 2nd april");
+		System.out.println("Shivani  kissd harshit on 29th juneg");
+		System.out.println("Shivani  kissd harshit on 29th juneg");
+g
 	}
+
 
 }
